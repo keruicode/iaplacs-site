ba51d19977854d1a5d4bc1e7a3a4b8d6c73bd560
keruicode <2602005529@qq.com>
2026-07-26 07:38:51 +0800

    Align national map coordinate labels

--- tools/render_worknx_national_overview.sh
+++ tools/render_worknx_national_overview.sh
@@ -83,8 +83,8 @@ caption_panel() {
     -fill black \
     -stroke none \
     -font "times.ttf" \
-    -pointsize 40 \
-    -draw 'text 0,84 "50°N" text 0,187 "40°N" text 0,290 "30°N" text 0,393 "20°N"' \
+    -pointsize 32 \
+    -draw 'text 76,82 "50°N" text 76,185 "40°N" text 76,288 "30°N" text 76,391 "20°N"' \
     -gravity North \
     -background white \
     -splice 0x92 \
